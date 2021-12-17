1b68672a3183df4045f55987438f3d41326f0bd8
ddochi <12.12kimiyeon@gmail.com>
2021-12-17 13:56:59 +0900

    interface

---
+++ src/Interface/Ground.java
@@ -0,0 +1,15 @@
+package Interface;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ddochi
+ * Date: 2021-12-17
+ * Time: 오후 1:52
+ * Luck is the residue of design. -Branch Rickey
+ * <PRE>exercise_book|</PRE>
+ */
+public interface Ground {
+
+    double getGround ();
+
+}
